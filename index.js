850f57f3985db80e2d828b4713e3c7f37956a228
Nick Flores-Tober <nflores161@gmail.com>
2021-04-25 20:52:28 -0400

    commit tree 4498de5ee1720d986e6d309fe14c583f4ea8153d

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+
+function findMatching(drivers, string) {
+  return drivers.filter(matchingDriver => matchingDriver.toLowerCase() === string.toLowerCase());
+}
+
+function fuzzyMatch(drivers, string) {
+  return drivers.filter(driversName => driversName.toLowerCase().indexOf(string.toLowerCase()) === 0)
+}
+
+function matchName(drivers, string) {
+  return drivers.filter(matchingDriver => matchingDriver.name === string)
+}
